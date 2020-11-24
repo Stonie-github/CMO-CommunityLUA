85690c0b12d6657278fa094e77868adff4b6e545
Steinar Thorvaldsson <steinar.thorvaldsson@gmail.com>
2020-11-24 22:09:50 +0100

    Mission dispatch complete
    Version 0.1 more or less ready

--- Authors/Stonie/StonieAI/Src/AutoHAVCAP/AutoHAVCAP.lua
+++ Authors/Stonie/StonieAI/Src/AutoHAVCAP/AutoHAVCAP.lua
@@ -54,16 +54,31 @@ function estimate_intercept_threat_vs_asset (threat, hav_asset, cap_time_to_area
 end
 
 -- Launch a HAVCAP mission with Unit: combat_ac, towards Unit:hva_ac, mission belongs to Side:script_side and mission name is a string
-function lunch_havcap_mission(combat_ac, hva_ac, script_side, mission_name)
+function lunch_havcap_mission(combat_ac, hva_ac, script_side, mission_name,parameters)
+    local HAVUnit = ScenEdit_GetUnit(hva_ac)
 
     -- Add a tracking reference point 1 mile in front of unit
-    HAVUnit = ScenEdit_GetUnit(hva_ac)
-    local havLocation = ScenEdit_AddReferencePoint( {side=script_side.name, RelativeTo=HAVUnit.name, bearingtype='rotating', bearing=HAVUnit.heading ,distance=5,highlighted=true })
-
+    local havLocation = ScenEdit_AddReferencePoint( {side=script_side.name, RelativeTo=HAVUnit.name, bearingtype='rotating', bearing=HAVUnit.heading ,distance=5,clear=true })
+    
     -- Add a mission for the HAVCAP asset based on the reference point, Assign CAP
-    CAPUnit = ScenEdit_GetUnit(combat_ac)
+    local mission_config = { oneThirdRule=false, checkOPA=false, transitThrottleAircraft='Military', useFlightSize=false, attackDistanceAircraft=tostring(parameters.threat_zone) }
     local script_mission = ScenEdit_AddMission(script_side.name,mission_name, 'patrol',{type='AAW', zone={havLocation.name}})
-    local status = ScenEdit_AssignUnitToMission(CAPUnit.name, script_mission.name)
+    local script_mission = ScenEdit_SetMission(script_side.name,mission_name,mission_config)
+    
+    local dispatched = 0
+    local num_ac = #combat_ac
+    local loop_counter = 1
+    repeat -- Cycle through cap list but quit when dispatch or end of available assets is reached
+        local CAPUnit = ScenEdit_GetUnit(combat_ac[loop_counter])
+        if(CAPUnit.readytime == '0') then -- Check if ready
+            --Dispatch AC to mission
+            ScenEdit_AssignUnitToMission(CAPUnit.name, mission_name)
+            dispatched = dispatched + 1
+            --print("Dispatching A/C to mission")
+        end
+        
+        loop_counter = loop_counter + 1
+    until ((loop_counter == num_ac) or (dispatched == parameters.dispatch_size))
 end
 
 -- Populate the table passed to this function with air contacts with contacts from side.contacts
@@ -89,12 +104,12 @@ function sai_havcap_evaluate_immediate_threats(hva_asset_list, threatlist, launc
         local hav_unit = ScenEdit_GetUnit(v)
 
         if (hav_unit.condition == 'Airborne') then  -- Skip flights that are not airborne
-            print("Checking threat against asset " .. v.name)
+            --print("Checking threat against asset " .. v.name)
             local threatened = false
 
             for k,v in ipairs(threatlist) do
                 local threat = VP_GetContact({guid=v.guid})
-                print("Checking " .. threat.name)
+                --print("Checking " .. threat.name)
                 local is_threatened = estimate_intercept_threat_vs_asset(threat, hav_unit, launch_parameters.cap_time_to_hav, launch_parameters.cap_time_margin, launch_parameters.threat_zone)
                 -- Only one threat is needed to give threatened status
                 if (is_threatened == true) then
@@ -104,7 +119,7 @@ function sai_havcap_evaluate_immediate_threats(hva_asset_list, threatlist, launc
             table.insert(ret, threatened) 
         else
             table.insert(ret, false) 
-            print("Skipping Non Airborne Unit")
+            --print("Skipping non-airborne asset")
         end
     end
 
@@ -117,7 +132,7 @@ function sai_update_threat_counters(hva_asset_list, threatened_assets, hva_threa
     local str = hva_threat_counter_global_name_string
     -- Create global variable with threat counters
     if (_G[str] == nil) then
-        print("Creating threat counters")
+        --print("Creating threat counters")
         _G[str] = {}
         for k,v in ipairs(hva_asset_list) do
             _G[str][k] = 0
@@ -125,6 +140,7 @@ function sai_update_threat_counters(hva_asset_list, threatened_assets, hva_threa
     end
     --print(hva_threat_counters)
 
+    --Increment threat counters
     for k,v in ipairs(threatened_assets) do
         if(v) then
             _G[str][k] = _G[str][k] + 1
@@ -138,25 +154,51 @@ end
 function sai_havcap_check_mission_go(mission, parameters)
     for k,v in ipairs(_G[mission]) do
         if(v >= parameters.threat_persistence_trigger_count) then
-            print("Threat established!")
+            --print("Threat established!")
             return true
         end
     end
+end
+
+-- Dispatch a mission to defend a asset
+function sai_dispatch_mission(CAPLIST, HVALIST, script_side, actual_mission_name, mission_var, parameters)
+    if ((mission_var.mission_launched == false) or (mission_var.mission_launched == nil)) then
+        print("Launcing mission")
+        lunch_havcap_mission(CAPLIST, HVALIST[1], script_side, actual_mission_name, parameters) -- FIX selection of HVA to laucn
+        mission_var.mission_launched = true
+    else
+        print("No mission launched")
+    end
+end
+
+-- Create mission variables if they dont exist NOT USED ATM
+function sai_havcap_restore_mission_vars(mission_var, parameters)
+    local str = mission_var
+    -- Create global variable with threat counters
+    if (_G[str] == nil) then
+        --print("Creating threat counters")
+        _G[str] = {}
+        for k,v in ipairs(hva_asset_list) do
+            _G[str][k] = 0
+        end
+        table.insert(_G[str].parameters, parameters)
+    end
+   
 end
 
 -------------------- START OF SCRIPT --------------------------------------------------------
 SCRIPT_SIDE = "Opfor"
 --------------------------------------------- ASSETS --------------------------------------
 local HVALIST = { {name='SIGINT #1', side=SIDENAME}, {name="SIGINT #2", side="Opfor"}, { name="SIGINT #3", side="opfor" }, {name="SIGINT #4", side = "Opfor" }}
-local CAPLIST = { {name="Rusky #1", side="Opfor"}, {name = "Rusky #2", side="Opfor"}}
-local MISSION_NAME = "HVAmission1" -- Must be unique!
-
+local CAPLIST = { {name="Rusky #1", side="Opfor"}, {name = "Rusky #2", side="Opfor"}, {name = "Rusky #3", side="Opfor"}, {name = "Rusky #4", side="Opfor"}, {name = "Rusky #5", side="Opfor"}, {name = "Rusky #6", side="Opfor"}}
+local SCRIPT_MISSION = "HVAmission1" -- Must be unique!
+local ACTUAL_MISSION_NAME = "HAVCAP MISSION"
+--HVAmission1 = nil  -- Uncomment and run to reset mission status
 --------------------------------------------- CONFIG ---------------------------------------
-local parameters = { cap_time_to_hav=0.11, cap_time_margin=0.023, threat_zone=50, threat_persistence_trigger_count=4}
+local parameters = { cap_time_to_hav=0.11, cap_time_margin=0.023, threat_zone=50, threat_persistence_trigger_count=4, dispatch_size=2}
 
 --------------------------------------------- MAIN ---------------------------------
 local script_side = VP_GetSide({name=SCRIPT_SIDE})
-local dispatch_size = 2 -- How many CAPs
 
 -- Populate potential air threats
 local threatlist = {}
@@ -167,17 +209,12 @@ local threat_count = sai_populate_air_threat_list(threatlist, script_side.contac
 local threatened_assets = sai_havcap_evaluate_immediate_threats(HVALIST, threatlist, parameters)
 
 -- Update threat counters
-sai_update_threat_counters(HVALIST, threatened_assets, MISSION_NAME)
+sai_update_threat_counters(HVALIST, threatened_assets, SCRIPT_MISSION)
 
 -- Check for mission go
-local mission_go = sai_havcap_check_mission_go(MISSION_NAME, parameters)
+local mission_go = sai_havcap_check_mission_go(SCRIPT_MISSION, parameters)
 
 --print(HVAmission1)
-
-if ((mission_go == true) and ((mission_launched == false) or (mission_launched == nil))) then
-    print("Launcing mission")
-    mission_launched = true
-    lunch_havcap_mission(CAPLIST[2], HVALIST[1], script_side, "HAVCAP MISSION")
-else
-    print("No mission launched")
+if (mission_go == true) then
+    sai_dispatch_mission(CAPLIST, HVALIST, script_side, ACTUAL_MISSION_NAME,HVAmission1,parameters)
 end
